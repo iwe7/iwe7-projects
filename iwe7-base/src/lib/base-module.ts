540e544339c48a39e86f75a4ff0f86f5da632966
iwe7 <admin@meepo.com.cn>
2018-05-16 10:30:23 +0800

    rebuild

--- iwe7-base/src/lib/base-module.ts
+++ iwe7-base/src/lib/base-module.ts
@@ -1,7 +1,7 @@
 import { Injector } from "@angular/core";
 import { GlobalService } from "./global.service";
 export abstract class Iwe7BaseModule {
-  constructor(private injector: Injector) {
+  constructor(public injector: Injector) {
     const _g = this.injector.get(GlobalService);
     _g.init(injector);
   }
