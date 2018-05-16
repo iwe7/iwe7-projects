296106973406cd51068290338637ded1979919e2
iwe7 <admin@meepo.com.cn>
2018-05-16 10:21:45 +0800

    rebuild

---
+++ iwe7-base/src/lib/global.service.ts
@@ -0,0 +1,63 @@
+import {
+  Injectable,
+  Type,
+  Injector,
+  ComponentFactoryResolver
+} from "@angular/core";
+import { NgElementConstructor } from "@angular/elements";
+import { Observable, of } from "rxjs";
+import { fromPromise } from "rxjs/observable/fromPromise";
+import { createCustomElement } from "@angular/elements";
+declare const window: any;
+
+export function factoryGlobalService() {
+  (<any>window).GlobalService =
+    (<any>window).GlobalService || new GlobalService();
+  return (<any>window).GlobalService;
+}
+
+@Injectable({
+  providedIn: "root",
+  useFactory: factoryGlobalService
+})
+export class GlobalService {
+  map: Map<string, NgElementConstructor<any>> = new Map();
+
+  constructor() {}
+
+  init(injector: Injector) {
+    const factoryResolver = injector.get(ComponentFactoryResolver);
+    const _factories = (<any>factoryResolver)._factories;
+    _factories.forEach((item, key) => {
+      this.registerElement(item.selector, item.componentType, injector);
+    });
+  }
+
+  set(key: string, item: NgElementConstructor<any>) {
+    this.map.set(key, item);
+  }
+
+  get(key: string): NgElementConstructor<any> {
+    return this.map.get(key);
+  }
+
+  registerElement(
+    selector: string,
+    type: Type<any>,
+    injector: Injector
+  ): Observable<string> {
+    let element = customElements.get(selector);
+    if (!element) {
+      let ngElementConstructor = createCustomElement(type, {
+        injector: injector
+      });
+      customElements.define(selector, ngElementConstructor);
+      this.set(selector, ngElementConstructor);
+      return fromPromise(
+        customElements.whenDefined(selector).then(res => selector)
+      );
+    } else {
+      return of(selector);
+    }
+  }
+}
--- iwe7-base/src/public_api.ts
+++ iwe7-base/src/public_api.ts
@@ -1,3 +1,4 @@
 export * from "./lib/base-module";
 export * from "./lib/base-component";
 export * from "./lib/base-directive";
+export * from "./lib/global.service";
